1ba5e32816c03cc3c4dc423e3207954679ac204f
Halimah <64546668+devhalimah@users.noreply.github.com>
2020-08-10 16:42:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="./css/form.css">
   </head>
   <body>
-    <form action="form-process.php" method="POST" autocomplete="off" target="blank">
+    <form action="form_process.php" method="POST" autocomplete="off" target="blank">
 	<h2> USER BIO-DATA </h2>
 	   <input type="text" name="firstname" placeholder="First Name" required>
 	   <input type="text" name="lastname" placeholder="Last Name" required>
